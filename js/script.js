411e0f38b69836dd12caaf75e6a58181a43cf985
Lucas <83518257+luqaska@users.noreply.github.com>
2022-03-17 18:37:19 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var estado;
+function checkearEstado(){
+  try{
+    fetch('https://ghost.television.si/data/index.json')
+      .then(response => response.json())
+      .then(function(data){
+        if(data["status"]==1){estado="Online";}else if(data["status"]==0){estado="Offline";}else{estado="Error";}
+        document.getElementById("estado").innerHTML = "<h2>"+estado+"</h2>";
+      });
+  }
+  catch(err){
+    document.getElementById("estado").innerHTML = "<iframe src='https://ghost-systems.lu700.repl.co/estado.php' style='border:none;height:50px'></iframe>";
+  }
+}
+checkearEstado();
+setInterval(checkearEstado, 5000);
